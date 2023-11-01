6c30645405745895416c955bdfdd519d28964a21
jens-sam <114012084+jens-sam@users.noreply.github.com>
2023-11-01 15:02:38 -0600

    Update dft_fft_pythontesting.py

--- Fast_Fourier_Transform/dft_fft_pythontesting.py
+++ Fast_Fourier_Transform/dft_fft_pythontesting.py
@@ -10,25 +10,43 @@
 # print("DFT of the array from 1 to 16 is:")
 # print(dft_result)
 
+# import numpy as np
+
+# def simulated_fft(x):
+#     N = len(x)
+#     if N <= 1:
+#         return x
+#     even = simulated_fft(x[0::2])
+#     odd = simulated_fft(x[1::2])
+#     T = [np.exp(-2j * np.pi * k / N) * odd[k] for k in range(N // 2)]
+#     return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+
+# # Test the function with a random array
+# N = 16  # Make sure N is a power of 2 for radix-2 FFT
+# x = np.random.rand(N) + 1j * np.random.rand(N)
+
+# # Compare the simulated FFT with NumPy's FFT
+# X_my = simulated_fft(x)
+# X_np = np.fft.fft(x)
+
+# # Check if the outputs are almost equal
+# np.testing.assert_array_almost_equal(X_my, X_np)
+# print("Outputs are almost equal!")
+
 import numpy as np
 
-def simulated_fft(x):
-    N = len(x)
-    if N <= 1:
-        return x
-    even = simulated_fft(x[0::2])
-    odd = simulated_fft(x[1::2])
-    T = [np.exp(-2j * np.pi * k / N) * odd[k] for k in range(N // 2)]
-    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+def test_fft_python(N):
+    # Initialize a test array with the same values used in the C implementation
+    x = [np.exp(2j * np.pi * i / N) for i in range(N)]
 
-# Test the function with a random array
-N = 16  # Make sure N is a power of 2 for radix-2 FFT
-x = np.random.rand(N) + 1j * np.random.rand(N)
+    # Apply FFT using NumPy's FFT function
+    fft_result = np.fft.fft(x)
 
-# Compare the simulated FFT with NumPy's FFT
-X_my = simulated_fft(x)
-X_np = np.fft.fft(x)
+    # Print the results
+    print(f"Python FFT Results for N = {N}:")
+    for value in fft_result:
+        print(f"{value.real} + {value.imag}i")
 
-# Check if the outputs are almost equal
-np.testing.assert_array_almost_equal(X_my, X_np)
-print("Outputs are almost equal!")
+if __name__ == "__main__":
+    N = 16  # Same size used in the C test
+    test_fft_python(N)
