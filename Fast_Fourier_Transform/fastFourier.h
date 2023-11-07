4a58d857e4be38f2598990a434defbfaf6a6e682
jens-sam <114012084+jens-sam@users.noreply.github.com>
2023-11-07 11:13:21 -0700

    Update fastFourier.h
    added fft but again not working only speed working

--- Fast_Fourier_Transform/fastFourier.h
+++ Fast_Fourier_Transform/fastFourier.h
@@ -6,6 +6,10 @@ extern "C" {
 #endif
 
 void dft(const double *in_real, const double *in_imag, double *out_real, double *out_imag, int N);
+// not working fft
+void fft(const double *in_re, const double *in_im, double *out_re, double *out_im, int N);
+
+
 
 #ifdef __cplusplus
 }
